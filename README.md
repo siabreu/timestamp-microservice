125a3a208addac819bfe25a417e92bb7108f3a64
Replit user <>
2023-06-05 10:53:56 -0300

    creates timestamp converter

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Timestamp Microservice
 
+Faça o clone do projeto
+
+utilize o yarn para executar
+`yarn start`
+
 This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
+function isTimestamp(value){
+	return /^[0-9]+$/.test(value)
+}
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api', (req, res) => {
+	const currentDate = new Date().toUTCString()
+	const currentUnix = Date.parse(currentDate)
+	res.json({ unix: currentUnix, utc: currentDate })
+})
 
+app.get('/api/:date?', (req, res) => {
+ const dateString = req.params.date
+ 
+ if (isTimestamp(dateString)) {
+	 const timestamp = parseInt(dateString)
+   const utcDate = new Date(timestamp).toUTCString()
 
+   res.json({ unix: timestamp, utc: utcDate })
+ } 
+ else {
+	 const timestamp = Date.parse(dateString)
+   const utcDate = new Date(timestamp).toUTCString()
+
+   if (timestamp)
+		 res.json({ unix: timestamp, utc: utcDate })
+	 else
+		 res.json({ error: "Invalid Date" })
+	}
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
